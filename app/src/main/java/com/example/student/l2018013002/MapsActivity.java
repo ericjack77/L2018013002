0373d974f9837e1a140b04eafe059b15ba67fea1
ericchiu <ericjack7@gmail.com>
2018-01-30 15:12:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/l2018013002/MapsActivity.java
@@ -0,0 +1,48 @@
+package com.example.student.l2018013002;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sanchung = new LatLng(25.0621, 121.4979);
+        LatLng tp101 = new LatLng(25.0336,121.5646);
+        mMap.addMarker(new MarkerOptions().position(sanchung).title("Marker in Sanchung"));
+        mMap.addMarker(new MarkerOptions().position(tp101).title("Marker in tp101"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sanchung,15));
+    }
+}
